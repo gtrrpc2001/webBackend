95f62f325ad2851d373b3ab86ea8b3398340e4a4
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-12-08 15:05:48 +0900

    Update ecg_csv_ecgdata.service.ts

--- src/service/ecg_csv_ecgdata.service.ts
+++ src/service/ecg_csv_ecgdata.service.ts
@@ -102,4 +102,38 @@ try{
     return Value;    
     } 
 
+  async getEcgTime(empid:string,startDate:string,endDate:string): Promise<string[]>{        
+        try{
+           const result = await this.ecg_csv_ecgdataRepository.createQueryBuilder('ecg_csv_ecgdata')
+                                .select('Mid(writetime,12,4) writetime')                                
+                                .where({"eq":empid})
+                                .andWhere({"writetime":MoreThanOrEqual(startDate)})
+                                .andWhere({"writetime":LessThan(endDate)})
+                                .groupBy('Mid(writetime,12,4)')
+                                .getRawMany()                    
+          console.log(empid)                                                    
+          return result;    
+        } catch(E){
+            console.log(E)
+        }                 
+      
+     }
+
+     async getGraphEcgValue(empid:string,startDate:string,endDate:string): Promise<number[]>{        
+        try{
+           const result = await this.ecg_csv_ecgdataRepository.createQueryBuilder('ecg_csv_ecgdata')
+                                .select('ecgpacket')                                
+                                .where({"eq":empid})
+                                .andWhere({"writetime":MoreThanOrEqual(startDate)})
+                                .andWhere({"writetime":LessThanOrEqual(endDate)})                                
+                                .getRawMany()                                               
+          const changeEcg:number[] = await commonFun.getEcgNumArr(result)  
+        //   const Value = (result.length != 0 && empid != null)? changeEcg : [0]                                
+          return changeEcg;    
+        } catch(E){
+            console.log(E)
+        }                 
+      
+     }
+
 }
