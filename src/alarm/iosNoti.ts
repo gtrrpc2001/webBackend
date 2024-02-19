85cf8df071d5e36fb9d6e8d1dab72112e42b6c37
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-02-19 10:54:14 +0900

    Update iosNoti.ts

--- src/alarm/iosNoti.ts
+++ src/alarm/iosNoti.ts
@@ -1,33 +1,33 @@
-import * as admin from 'firebase-admin';
-import { staticConfigValue } from 'src/config/staticConfigValue';
-import { ConfigService } from '@nestjs/config';
-import { firebasenoti } from './firebasenoti';
+// import * as admin from 'firebase-admin';
+// import { staticConfigValue } from 'src/config/staticConfigValue';
+// import { ConfigService } from '@nestjs/config';
+// import { firebasenoti } from './firebasenoti';
 
-export class iosNoti{
+// export class iosNoti{
 
-    static iosPath = ""
-    static ios:any
+//     static iosPath = ""
+//     static ios:any
 
-    static setPath = (configService:ConfigService)  => {        
-        this.iosPath = staticConfigValue.getFirebase_sdk_ios(configService).path   
-        this.ios = require(this.iosPath)
-    }    
+//     static setPath = (configService:ConfigService)  => {        
+//         this.iosPath = staticConfigValue.getFirebase_sdk_ios(configService).path   
+//         this.ios = require(this.iosPath)
+//     }    
 
-    static async IOS(configService:ConfigService): Promise<boolean>{
-        try{           
-          if(admin.apps.length != 0)
-                admin.app().delete();
+//     static async IOS(configService:ConfigService): Promise<boolean>{
+//         try{           
+//           if(admin.apps.length != 0)
+//                 admin.app().delete();
 
-          if(this.iosPath == "")
-            this.setPath(configService);            
+//           if(this.iosPath == "")
+//             this.setPath(configService);            
             
-          firebasenoti.initializeApp(this.ios)
+//           firebasenoti.initializeApp(this.ios)
 
-          return true    
-          }catch(E){
-            console.log('여기서 빠짐')           
-            console.log(E) 
-             return false
-          }
-    }
-}
+//           return true    
+//           }catch(E){
+//             console.log('여기서 빠짐')           
+//             console.log(E) 
+//              return false
+//           }
+//     }
+// }
