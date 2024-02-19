2f5f3672b560fa316c1df8b3c65b9141c53dc10e
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-02-19 10:53:40 +0900

    Update firebasenoti.ts

--- src/alarm/firebasenoti.ts
+++ src/alarm/firebasenoti.ts
@@ -1,5 +1,5 @@
 import { isDefined } from 'class-validator';
-import * as firebase from 'firebase-admin';
+// import * as firebase from 'firebase-admin';
 import { ecg_csv_ecgdataDTO } from 'src/dto/ecg_csv_ecgdata.dto';
 import { staticConfigValue } from 'src/config/staticConfigValue';
 import { ConfigService } from '@nestjs/config';
@@ -17,11 +17,11 @@ export class firebasenoti{
 
 static async PushNoti(tokens:string[],body:ecg_csv_ecgdataDTO,configService:ConfigService): Promise<boolean>{
     try{
-      const names = ["IOS","ANDROID"]
-      for(var name of names){        
-        const bool = await this.setKindBrand(name,configService)
-        if(bool) await this.setPushAlarm(tokens,body.arrStatus,body.writetime,body.address,body.bodystate,body.timezone) 
-      }
+      // const names = ["IOS","ANDROID"]
+      // for(var name of names){        
+      //   const bool = await this.setKindBrand(name,configService)
+      //   if(bool) await this.setPushAlarm(tokens,body.arrStatus,body.writetime,body.address,body.bodystate,body.timezone) 
+      // }
       return true
     } catch(E){        
       console.log(E)
@@ -30,51 +30,51 @@ static async PushNoti(tokens:string[],body:ecg_csv_ecgdataDTO,configService:Conf
     }          
 }
 
-static setKindBrand = async(name:string,configService:ConfigService):Promise<boolean> => {
-  try{
-    switch(name){
-      case "ANDROID" :
-        await androidNoti.ANDROID(configService,firebase)
-        break;
-      default :      
-        await iosNoti.IOS(configService,firebase);        
-        break;
-    }
+// static setKindBrand = async(name:string,configService:ConfigService):Promise<boolean> => {
+//   try{
+//     switch(name){
+//       case "ANDROID" :
+//         await androidNoti.ANDROID(configService,firebase)
+//         break;
+//       default :      
+//         await iosNoti.IOS(configService,firebase);        
+//         break;
+//     }
 
-    return true;
-  } catch(E){
-    console.log(E)
-    return false
-  } 
-}
+//     return true;
+//   } catch(E){
+//     console.log(E)
+//     return false
+//   } 
+// }
 
-static async setPushAlarm(tokens:string[],arrStatus:string,time:string,address:string,bodystate:number,timezone:string): Promise<boolean>{
-  try{                          
-   let title = alarmController.getTitle(arrStatus,bodystate,timezone)
-   let body = alarmController.getBody(address,time,timezone)    
-   console.log('성공 ' + title)     
-    await firebase
-    .messaging()
-    .sendEachForMulticast({
-      notification: {title,body},
-      tokens: tokens,
-      android: {priority:'high'},
-      apns:{
-        payload:{
-          aps:{
-            sound: bodystate == 1 ? 'heartAttackSound.wav' :'basicsound.wav'
-          }                
-        }
-      }
-    })
-    .catch((error: any) => {
-      console.error(error)
-      return false;
-    })
-    return true;
-  }catch(E){
-    console.log(E)
-    return false;
-  }    
-}
+// static async setPushAlarm(tokens:string[],arrStatus:string,time:string,address:string,bodystate:number,timezone:string): Promise<boolean>{
+//   try{                          
+//    let title = alarmController.getTitle(arrStatus,bodystate,timezone)
+//    let body = alarmController.getBody(address,time,timezone)    
+//    console.log('성공 ' + title)     
+//     await firebase
+//     .messaging()
+//     .sendEachForMulticast({
+//       notification: {title,body},
+//       tokens: tokens,
+//       android: {priority:'high'},
+//       apns:{
+//         payload:{
+//           aps:{
+//             sound: bodystate == 1 ? 'heartAttackSound.wav' :'basicsound.wav'
+//           }                
+//         }
+//       }
+//     })
+//     .catch((error: any) => {
+//       console.error(error)
+//       return false;
+//     })
+//     return true;
+//   }catch(E){
+//     console.log(E)
+//     return false;
+//   }    
+// }
 }
