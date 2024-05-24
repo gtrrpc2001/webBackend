15383b07decc66f0e48df92db3520bbecbaa473e
gtrrpc2001 <cbdgjs3548@naver.com>
2024-05-24 14:18:20 +0900

    arr module update

--- src/module/ecg_csv_ecgdata_arr.module.ts
+++ src/module/ecg_csv_ecgdata_arr.module.ts
@@ -4,13 +4,13 @@ import { ecg_csv_ecgdata_arrController } from 'src/controller/ecg_csv_ecgdata_ar
 import { ecg_byteEntity } from 'src/entity/ecg_byte.entity';
 import { ecg_csv_ecgdata_arrEntity } from 'src/entity/ecg_csv_ecgdata_arr.entity';
 import { parentsEntity } from 'src/entity/parents.entity';
-import { userEntity } from 'src/entity/user.entity';
+import { UserEntity } from 'src/entity/user.entity';
 import { ecg_csv_ecgdata_arrService } from 'src/service/ecg_csv_ecgdata_arr.service';
 
 
 @Module({
     imports:[
-        TypeOrmModule.forFeature([ecg_csv_ecgdata_arrEntity,parentsEntity,userEntity,ecg_byteEntity])
+        TypeOrmModule.forFeature([ecg_csv_ecgdata_arrEntity,parentsEntity,UserEntity,ecg_byteEntity])
     ],
     controllers:[ecg_csv_ecgdata_arrController],
     providers:[ecg_csv_ecgdata_arrService]
